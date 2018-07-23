c34c7c9e3cdbdd08b05f18b949cfb265d8cf5ede
Yonatan Kra <yonatan.kr@walkme.com>
2018-07-23 14:35:45 +0300

    Added the canvas module and its component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CanvasModule} from './canvas/canvas.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CanvasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
