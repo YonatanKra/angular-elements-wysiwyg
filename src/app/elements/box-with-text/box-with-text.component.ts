dfc9b9134d1fb5f768a00fd4f7bed51e59d2d6e4
8a211864ea14d428d926a81a929a18d0b8ada3e6 <kra.yonatan@gmail.com>
2018-08-02 09:40:15 +0300

    Tests pass

--- src/app/elements/box-with-text/box-with-text.component.ts
+++ src/app/elements/box-with-text/box-with-text.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, Output, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-box-with-text',
   template: `
-    <p>
-      box-with-text works!
-    </p>
+    <div class="box-with-text" [ngStyle]="boxStyles">
+      {{elementData.text}}
+    </div>
   `,
   styleUrls: ['./box-with-text.component.css']
 })
-export class BoxWithTextComponent implements OnInit {
+export class BoxWithTextComponent {
 
-  constructor() { }
+  private wrapper;
 
-  ngOnInit() {
+  public _elementData;
+  public boxStyles = {
+    color: 'black',
+    border: ``,
+    'border-radius': ``,
+    backgroundColor: `white`,
+    width: `200`,
+    height: `200`,
+  };
+
+  @Input()
+  set elementData(data) {
+    this._elementData = data;
+    this.updateBox(data);
   }
 
+  get elementData() {
+    return this._elementData ? this._elementData : {};
+  }
+
+  constructor(private elRef: ElementRef) {
+  }
+
+  private updateBox(elementData) {
+    this.boxStyles.color = elementData.textColor;
+    this.boxStyles.border = `1px solid ${elementData.borderColor}`;
+    this.boxStyles['border-radius'] = `${elementData.borderRadius}`;
+    this.boxStyles.backgroundColor = `${elementData.backgroundColor}`;
+    this.boxStyles.width = `${elementData.width}`;
+    this.boxStyles.height = `${elementData.height}`;
+  }
 }
