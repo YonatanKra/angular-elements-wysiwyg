a259f8bbc9acbae021a1bab8348b5adc599c7177
8a211864ea14d428d926a81a929a18d0b8ada3e6 <kra.yonatan@gmail.com>
2018-09-02 09:00:04 +0300

    Data service tests pass

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,37 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {ElementSpecs} from '../elements/models/element-specs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private _canvasData: Array<ElementSpecs> = [];
+  private _createData() {
+    const data = [];
+    for (let i = 0; i < Math.random() * 100 + 10; i++) {
+      data.push(new ElementSpecs());
+      data[i].type = Math.random().toString();
+    }
+    return data;
+  }
+
+  public canvasData = new EventEmitter();
+
   constructor() {
 
+  }
+
+  updateCanvasData(data?: Array<ElementSpecs>) {
+    this._canvasData = data;
+    this.canvasData.emit(data);
+  }
+
+  getCanvasData(cached?: boolean) {
+    let canvasData = this._canvasData;
+    if (cached !== true) {
+      canvasData = this._createData();
+    }
+    this.updateCanvasData(canvasData);
   }
 }
