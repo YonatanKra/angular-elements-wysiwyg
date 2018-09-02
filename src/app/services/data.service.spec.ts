e8a6fb227019a024f4d9c2ebd35fda073787d873
8a211864ea14d428d926a81a929a18d0b8ada3e6 <kra.yonatan@gmail.com>
2018-09-02 08:59:33 +0300

    Data service tests

---
+++ src/app/services/data.service.spec.ts
@@ -0,0 +1,65 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { DataService } from './data.service';
+import {ElementSpecs} from '../elements/models/element-specs';
+
+fdescribe('DataService', () => {
+  let service: DataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DataService]
+    });
+    service = TestBed.get(DataService);
+  });
+
+  function createData() {
+    const data = [];
+    for (let i = 0; i < Math.random() * 100 + 10; i++) {
+      data.push(new ElementSpecs());
+      data[i].type = Math.random().toString();
+    }
+    return data;
+  }
+
+  describe('update data', () => {
+    it('should emit an event with the new data', inject([DataService], (service: DataService) => {
+      spyOn(service.canvasData, 'emit').and.returnValue(true);
+      const data = createData();
+      service.updateCanvasData(data);
+      expect(service.canvasData.emit).toHaveBeenCalledWith(data);
+    }));
+
+    it('should cache the data', () => {
+      const data = createData();
+      service.updateCanvasData(data);
+      spyOn(service.canvasData, 'emit').and.returnValue(true);
+      service.getCanvasData(true);
+      expect(service.canvasData.emit).toHaveBeenCalledWith(data);
+    });
+  });
+
+  describe('get data', () => {
+    let data: Array<ElementSpecs>;
+    beforeEach(() => {
+      data = createData();
+      service.updateCanvasData(data);
+    });
+
+    it ('should emit new data', () => {
+      let newData = [];
+      service.updateCanvasData(data);
+      spyOn(service.canvasData, 'emit').and.callFake(someData => newData = someData);
+      service.getCanvasData();
+      expect(service.canvasData.emit).toHaveBeenCalledWith(newData);
+    });
+
+    it('should emit the cached data if param is true', () => {
+      let cachedData = [];
+      service.updateCanvasData(data);
+      spyOn(service.canvasData, 'emit').and.callFake(someData => cachedData = someData);
+      service.getCanvasData(true);
+      expect(service.canvasData.emit).toHaveBeenCalledWith(data);
+    });
+  });
+});
