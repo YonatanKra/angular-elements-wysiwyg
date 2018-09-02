a14dcf0176f41c55f6116bdd6b182e860e1ea16d
8a211864ea14d428d926a81a929a18d0b8ada3e6 <kra.yonatan@gmail.com>
2018-09-02 12:30:05 +0300

    Data service now mocks a better box-with-text

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,20 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {ElementSpecs} from '../elements/models/element-specs';
 
+const elementTypes = ['box-with-text'];
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  private _canvasData: Array<ElementSpecs> = [];
+  public _canvasData: Array<ElementSpecs> = [];
   private _createData() {
     const data = [];
     for (let i = 0; i < Math.random() * 100 + 10; i++) {
       data.push(new ElementSpecs());
-      data[i].type = Math.random().toString();
+      data[i].type = elementTypes[Math.round(Math.random() * (elementTypes.length - 1))];
+      data[i].position = {top: Math.random() * 400, left: Math.random() * 300};
+      data[i].data =  {text: Math.random().toString(), backgroundColor: 'red', height: 150 * Math.random() + 50, width: 150 * Math.random() + 320};
     }
     return data;
   }
